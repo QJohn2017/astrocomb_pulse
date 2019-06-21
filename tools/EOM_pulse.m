70f4caf0fbe86da322897cc2cdbb14fe29c76387
Ryan Petersburg <rpetersburg@gmail.com>
2019-06-21 17:57:22 -0400

    EOM_pulse uses modulation power

--- tools/EOM_pulse.m
+++ tools/EOM_pulse.m
@@ -1,17 +1,46 @@
-function u = EOM_pulse(t, f_mod, Vapp)
-%EOM_PULSE Summary of this function goes here
-%   Equation taken from https://www.osapublishing.org/ol/abstract.cfm?uri=ol-35-19-3234
-omega_m = 2*pi*f_mod;    %uWave mod frequency, 2*pi*hz
-%Vapp = 15;   %RF voltage applied to Phase Modulator, V
-V_pi = 3;   %Vpi of phase modulator, V
-phi_dc = pi/2;  %Phase set point of AM, radians
-vpi_ratio = 1/2; %1/2; %Drive Voltage of AM in units of Vpi
-%vpi_ratio = 2/3.9;
+function u = EOM_pulse(t, freq_m, P_IM, P_PM)
+% Produce a pulse from electro-optic intensity and phase modulators
+% Equation taken from https://www.osapublishing.org/ol/abstract.cfm?uri=ol-35-19-3234
+%
+% INPUT
+%
+% t - time series
+% freq_m - drive frequency in THz
+% P_IM - drive power of intensity modulators
+% P_PM - drive power of phase modulators
+% Vapp - Drive voltage applied to modulator, V
+%
+% OUTPUT
+% u - pulse at output
+
+% uWave mod frequency, 2*pi*hz
+omega_m = 2*pi * freq_m;    
+
+% Vpi of phase modulator, V
+V_pi_PM = 3;
+V_pi_IM = 3;
+
+% Convert power to voltage
+V_IM = 2 * 10^((P_IM-10)/20);
+V_PM = 2 * 10^((P_PM-10)/20);
+
+% Phase set point of IM, radians
+phi_dc = pi/2;  
+
+% Drive Voltage of IM in units of Vpi
+%vpi_ratio = 1/2;
+
+% Number of intensity and phase modulators
 N_IM = 1;
 N_PM = 2;
 
-E_IM = (1 + exp(1i*phi_dc).*exp(1i.*(pi*vpi_ratio).*cos(omega_m.*t))).^N_IM;    %Use 1 - exp... to center pulse
-E_PM = (exp(1i.*(pi*Vapp/V_pi).*cos(omega_m.*t))).^N_PM;
-u = E_IM.*E_PM;
-end % End function
+% Intensity modulator pulse
+E_IM = 1 + exp(1i * phi_dc + 1i * pi * V_IM / V_pi_IM .* cos(omega_m .* t));
 
+% Phase modulator pulse
+E_PM = exp(1i .* pi * V_PM / V_pi_PM .* cos(omega_m .* t));
+
+% Combine the pulses
+u = E_IM.^N_IM .* E_PM.^N_PM;
+
+end % End function
